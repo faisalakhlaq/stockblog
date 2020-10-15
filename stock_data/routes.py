60b0dfa634483a21037ba9e253a93662fa907e7c
Faisal Akhlaq <faisal.akhlaq@aol.com>
2020-10-15 20:12:17 +0200

    removed get tickers from gt

--- stock_data/routes.py
+++ stock_data/routes.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import render_template, request
-from get_all_tickers import get_tickers as gt
+# from get_all_tickers import get_tickers as gt
 
 from stock_data import app
 from stock_data.forms import StockDataForm
@@ -15,25 +15,28 @@ def home():
 
 @app.route('/stock_data', methods=['GET', 'POST'])
 def stock_data():
-    list_of_tickers = gt.get_tickers()
-    # first_tickers = ['DDD', 'MMM', 'WBAI', 'EGHT', 'AHC', 'AOS', 'ATEN', 'AIR', 'AAN', 'ABB', 'ABT', 'ABBV', 'ANF', 'AGD', 'AWP', 'ACP', 'JEQ', 'ASGI', 'AOD', 'ABM', 'AKR', 'ACEL', 'ACN', 'ACCO', 'ATV', 'AYI', 'GOLF', 'ADX', 'PEO', 'ADCT', 'AGRO', 'ADNT', 'ADT', 'ATGE', 'AAP', 'ADSW', 'WMS', 'ASIX', 'AVK', 'ACM', 'AEFC', 'AEB', 'AEG', 'AER', 'AJRD', 'AMG', 'MGR', 'MGRB', 'AFL', 'MITT', 'AGCO', 'A', 'AEM', 'ADC', 'AL', 'APD', 'ALG', 'AGI', 'ALK', 'AIN', 'ALB', 'ACI', 'AA', 'ALC', 'ALEX', 'ALX', 'ARE', 'AQN', 'AQNA', 'AQNB', 'BABA', 'Y', 'ATI', 'ALLE', 'ALE', 'ADS', 'AFB', 'AWF', 'AB', 'AIO', 'CBH', 'NCV', 'NCZ', 'ACV', 'NFJ', 'NIE', 'ALSN', 'ALL', 'ALLY', 'PINE', 'ALTG', 'AYX', 'ATUS', 'MO', 'ACH', 'ALUS', 'AMBC', 'ABEV', 'AMC', 'AMCR', 'AEE']
+    tickers = []
+    with open("tickers.txt", mode='r') as file:
+        for line in file.readlines():
+            tickers.append(line)
 
+    print(len(tickers))
     form = StockDataForm()
     if request.method == 'GET':
-        return render_template("stock_data.html", list_of_tickers=list_of_tickers, form=form)
+        return render_template("stock_data.html", list_of_tickers=tickers, form=form)
     elif request.method == 'POST':
         if not form.validate_on_submit() or \
                 form.startdate.data > form.enddate.data or \
                 form.startdate.data > datetime.date(datetime.now()):
             error_message = "Invalid Dates!"
-            return render_template("stock_data.html", list_of_tickers=list_of_tickers,
+            return render_template("stock_data.html", list_of_tickers=tickers,
                                    error_message=error_message, form=form)
 
-        stock_ticker = request.form.get('stock_ticker_list')
+        stock_ticker = (request.form.get('stock_ticker_list')).strip()
         start_date = form.startdate.data
         end_date = form.enddate.data
         data = StockAnalyzer().get_graph(stock_ticker, start_date, end_date)
-        return render_template("stock_data.html", list_of_tickers=list_of_tickers,
+        return render_template("stock_data.html", list_of_tickers=tickers,
                                script1=data[0], div1=data[1], cdn_js=data[2], form=form)
 
 
