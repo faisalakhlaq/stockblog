92ae2ed8603f4baf9ad4408b945aa1d2de589952
Faisal Akhlaq <faisal.akhlaq@aol.com>
2020-10-15 10:20:43 +0200

    config changed

--- stock_data/config.py
+++ stock_data/config.py
@@ -2,7 +2,6 @@ import os
 
 
 class Config:
-    DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     WTF_CSRF_ENABLED = True
     WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY')
