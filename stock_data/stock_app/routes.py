a8717d7ee540218c5474a6db2f55e847ff31d8c4
Faisal Akhlaq <faisal.akhlaq@aol.com>
2021-01-11 05:41:21 +0100

    introduced referesh button

--- stock_data/stock_app/routes.py
+++ stock_data/stock_app/routes.py
@@ -16,9 +16,23 @@ def home():
     return render_template('home.html')
 
 
-@stock.route('/stock_technical_terms', methods=['GET'])
+@stock.route('/stock_technical_terms', methods=['GET', 'POST'])
 def stock_technical_terms():
     technical_terms = StockTechnicalTerms.query.all()
+    if request.method == 'POST':
+        # import pdb; pdb.set_trace()
+        if request.form.get('refresh'):
+            pass
+        elif request.form.get('query'):
+            term_name_ = request.form.get('query')
+            if term_name_ and len(term_name_.strip()) > 0:
+                rs = StockTechnicalTerms.query.filter(StockTechnicalTerms.term_name.ilike("%" + term_name_ + "%")).all()
+                if len(rs) > 0:
+                    technical_terms = rs
+                else:
+                    flash('Sorry no terms found with the given name!')
+            else:
+                flash('Please provide a name!')
     return render_template("stock_technical_terms.html", terms=technical_terms)
 
 
