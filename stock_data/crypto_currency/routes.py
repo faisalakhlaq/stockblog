7d38ad1a528d97e2252c34f10ed757c30d46fbcb
Faisal Akhlaq <faisal.akhlaq@aol.com>
2021-01-26 05:33:44 +0100

    crypto frontend improvements

--- stock_data/crypto_currency/routes.py
+++ stock_data/crypto_currency/routes.py
@@ -26,21 +26,26 @@ def crypto_currency_data():
             # 1. Get the currencies from textarea
             # 2. Check if they are valid or send directly if the api won't crash with wrong symbols
             # 3. Put the returned data in a tabular form
-            txtarea = form.currencies.data
-            if txtarea is not None and len(txtarea.strip()) > 0:
-                amount = txtarea.split()
-                currencies = {}
-                seperator = '-'
-                for cur in amount:
-                    k = cur.split(seperator, 1)
-                    # v = cur.split(seperator, 1)[1]
-                    currencies[str(k[0]).upper()] = k[1]
-                try:
+            try:
+
+                txtarea = form.currencies.data
+                if txtarea is not None and len(txtarea.strip()) > 0:
+                    amount = txtarea.split()
+                    currencies = {}
+                    seperator = '-'
+                    for cur in amount:
+                        k = cur.split(seperator, 1)
+                        # v = cur.split(seperator, 1)[1]
+                        currencies[str(k[0]).upper()] = k[1]
+
                     data = CoinMarket().get_crypto_portfolio(currencies)
                     return render_template('crypto_fetcher.html', form=form,
                                            list_data=data, crypto_symbols=crypto_symbols)
-                except (ConnectionError, Timeout, TooManyRedirects, ValueError) as e:
-                    flash(str(e)) # TODO FIXME not being displayed
-                    print(e)
+            except IndexError as ie:
+                flash(str('Unable to fetch data. Please check your added currencies and amounts.')) # TODO FIXME not being displayed
+                print(ie)
+            except (ConnectionError, Timeout, TooManyRedirects, ValueError) as e:
+                flash(str(e)) # TODO FIXME not being displayed
+                print(e)
     return render_template('crypto_fetcher.html', form=form,
                            crypto_symbols=crypto_symbols)
